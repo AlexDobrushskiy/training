e198a331158b40751940043f062ff0898d7cf2a5
Alex Dobrushskiy <adobrushskiy@asdco.ru>
2013-08-19 21:18:45 +0400

    Solution for HW Unit 4 task 6

--- dz6.py
+++ dz6.py
@@ -3,88 +3,90 @@
 __author__ = 'Alex Dobrushskiy'
 
 from collections import deque
-
-f = open('file')
-
-actors = []
-for line in f:
-    words = line.split('\t')
-    actors.append((words[0], words[1], words[2].strip()))
-
-G = {}
-
+import random
+import csv
+from datetime import datetime
 
 def make_link(G, node1, node2):
-    if node1 == node2:
-        return
+    if node1 not in G:
+        G[node1] = {}
+    (G[node1])[node2] = 1
 
-    if node1 in G:
-        if node2 not in G[node1]:
-            G[node1].append(node2)
+    if node2 not in G:
+        G[node2] = {}
+    (G[node2])[node1] = 1
+
+    return G
+
+
+def read_graph(filename):
+    tsv = csv.reader(open(filename), delimiter='\t')
+    G = {}
+    actors = {}
+    movies = {}
+    for (actor, movie_name, year) in tsv:
+        movie = str(movie_name) + ", " + year
+        actors[actor] = 1
+        movies[movie] = 1
+        make_link(G, actor, movie)
+
+    return G, actors, movies
+
+
+def centrality(G, v):
+    distance_from_start = {}
+    distance_from_start[v] = 0
+    open_list = [v]
+    while len(open_list) > 0:
+        current = open_list.pop(0)
+        for neightbor in G[current].keys():
+            if neightbor not in distance_from_start:
+                distance_from_start[neightbor] = distance_from_start[current] + 1
+                open_list.append(neightbor)
+    return (float(sum(distance_from_start.values())))/len(distance_from_start)
+
+
+def rank(L, v):
+    r = 0
+    for l in L:
+        if l < v:
+            r += 1
+    return r
+
+
+def find_rank(L, i):
+    lt = {}
+    eq = {}
+    gt = {}
+    v = random.choice(L.keys())
+    for l in L.keys():
+        if L[l] < L[v]:
+            lt[l] = L[l]
+        elif L[l] == L[v]:
+            eq[l] = L[l]
+        elif L[l] > L[v]:
+            gt[l] = L[l]
+    if len(lt) >= i:
+        return find_rank(lt, i)
+    elif len(lt) + len(eq) >= i:
+        return v
     else:
-        G[node1] = [node2]
-
-    if node2 in G:
-        if node1 not in G[node2]:
-            G[node2].append(node1)
-    else:
-        G[node2] = [node1]
+        return find_rank(gt, i - len(lt) - len(eq))
 
 
-def distance(G, item):
-    to_visit = deque([item])
-    dist = {item: 0}
-    while to_visit:
-        current = to_visit.popleft()
-        for neightbor in G[current]:
-            if not neightbor in dist:
-                dist[neightbor] = dist[current] + 1
-                to_visit.append(neightbor)
-    return dist
-
-
-def avg_distance(G, item):
-    to_visit = deque([item])
-    dist = {item: 0}
-    result = 0
-    cnt = 1
-    while to_visit:
-        current = to_visit.popleft()
-        for neightbor in G[current]:
-            if not neightbor in dist:
-                dist[neightbor] = dist[current] + 1
-                result += dist[neightbor]
-                to_visit.append(neightbor)
-                cnt += 1
-    return result*1.0/cnt
-
-for item in actors:
-    make_link(G, item[0], item[1])
-
-new_G = {}
-
-for actor, film, date in actors:
-    if actor not in new_G:
-        for film in G[actor]:
-            for act in G[film]:
-                make_link(new_G, actor, act)
-
-avg_dst = []
+(G, actors, movies) = read_graph('file')
+centralities = {}
+lac = len(actors)
 cnt = 1
-len_G = len(new_G)
 
-for i in new_G:
-    print "Processing %d of %d" % (cnt, len_G)
-    avg_dst.append([i, avg_distance(new_G, i)])
+start =  datetime.now()
+for actor in actors.keys():
+    print "Proccessed %d from %d" % (cnt, lac)
+    centralities[actor] = centrality(G, actor)
     cnt += 1
+actor_index = find_rank(centralities, 20)
 
-sortedlist = sorted(avg_dst, key=lambda x: x[1])
-
-print sortedlist[:20]
-print sortedlist[0]
-print sortedlist[19]
-# print avg_distance(G, 'McClure, Marc (I)')
-# print len(distance(G, 'McClure, Marc (I)'))
-# print len(actors)
-# print len(G)
-# print actors[0]
+print start
+print datetime.now()
+print actor_index
+print centralities[actor_index]
