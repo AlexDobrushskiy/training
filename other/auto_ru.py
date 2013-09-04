8c30122f0911fe63a7e2fb2262bc5e02d6c5f4d6
Alex Dobrushskiy <adobrushskiy@asdco.ru>
2013-09-04 18:05:24 +0400

    Turn script to library

--- other/auto_ru.py
+++ other/auto_ru.py
@@ -13,35 +13,34 @@ import urllib
 import lxml.html
 
 
-page = urllib.urlopen('http://catalog.auto.ru/')
+def get_auto_dict():
+    page = urllib.urlopen('http://catalog.auto.ru/')
 
-doc = lxml.html.document_fromstring(page.read())
-
-table = doc.xpath('/html/body/div[1]/div[2]/article/div/table/tr/td[2]/div[2]/div[2]/div[2]/div/table')[0]
-
-brands = {}
-for i in table.getchildren():
-    for j in i.getchildren():
-        if j.text_content() != '&nbsp':
-            brand = j.text_content()
-            link = "http://catalog.auto.ru" + j.getchildren()[0].getchildren()[0].values()[0]
-            brands[brand] = link
-
-result = {}
-
-
-for b in brands:
-    page = urllib.urlopen(brands[b])
     doc = lxml.html.document_fromstring(page.read())
-    table = doc.xpath('/html/body/div[1]/div[2]/article/div/table/tr/td[2]/div[2]/div[3]/div[1]/table')[0]
 
-    result[b] = []
+    table = doc.xpath('/html/body/div[1]/div[2]/article/div/table/tr/td[2]/div[2]/div[2]/div[2]/div/table')[0]
+
+    brands = {}
     for i in table.getchildren():
         for j in i.getchildren():
             if j.text_content() != '&nbsp':
-                model = j.text_content()
-                result[b].append(model)
+                brand = j.text_content()
+                link = "http://catalog.auto.ru" + j.getchildren()[0].getchildren()[0].values()[0]
+                brands[brand] = link
+
+    result = {}
 
 
-for i in result:
-    print i, result[i]
+    for b in brands:
+        page = urllib.urlopen(brands[b])
+        doc = lxml.html.document_fromstring(page.read())
+        table = doc.xpath('/html/body/div[1]/div[2]/article/div/table/tr/td[2]/div[2]/div[3]/div[1]/table')[0]
+
+        result[b] = []
+        for i in table.getchildren():
+            for j in i.getchildren():
+                if j.text_content() != '&nbsp':
+                    model = j.text_content()
+                    result[b].append(model)
+
+    return result
