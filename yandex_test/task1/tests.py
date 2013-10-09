649da3d5c7529a896c117f3fe97a9cfb6d6319af
Alex Dobrushskiy <adobrushskiy@asdco.ru>
2013-10-09 16:43:48 +0400

    Long list test

--- yandex_test/task1/tests.py
+++ yandex_test/task1/tests.py
@@ -4,15 +4,28 @@ __author__ = 'Alex Dobrushskiy'
 
 import unittest
 from raws import *
+from random import choice
 
 
 class Task1Test(unittest.TestCase):
     def setUp(self):
         self.input_list = ["", "", "", "a", "b", "", "", "c", "", "d", "", "", ""]
 
-    def test_simple_case(self):
+    def test_sanity(self):
         result = proccess_raws(self.input_list)
         self.assertEqual(result, ["a", "b", "", "c", "", "d"])
+
+    def test_long_list(self):
+        elements = ["", "a", "", "b", "", "c"]
+        input = [choice(elements) for i in range(10000)]
+        result = proccess_raws(input)
+        self.assertNotEqual(result[0], "")
+        self.assertNotEqual(result[-1], "")
+        for i in range(len(result)):
+            if result[i] == "":
+                self.assertNotEqual(result[i-1], "")
+                self.assertNotEqual(result[i+1], "")
+
 
 if __name__ == '__main__':
     unittest.main()
